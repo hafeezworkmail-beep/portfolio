32375a5da270a9a348ca895c21f0dd3e3c55c276
abdulwasayhashmi3366 <abdulwasayhashmi6@gmail.com>
2025-08-13 15:04:21 +0500

    fourth revision

--- js/custom.js
+++ js/custom.js
@@ -270,5 +270,25 @@ $(document).ready(function () {
 
 });
 
+var splide = new Splide(".splide", {
+    perPage: 3,
+    gap: "2rem",
+    breakpoints: {
+        640: {
+            perPage: 2,
+            gap: ".7rem",
+            height: "6rem"
+        },
+        480: {
+            perPage: 1,
+            gap: ".7rem",
+            height: "6rem"
+        }
+    }
+});
+
+splide.mount();
+
+
 // video pop up end
 
